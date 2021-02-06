a42bc390e2cd63f990ca30957fc0b51592639bca
Saduk0 <saduk0@protonmail.com>
2021-02-06 22:00:50 +0300

    Went through getting started and tutorial

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +10,34 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World!'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath %s' % escape(subpath)
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+@app.route('/about')
+def about():
+    return 'The about page'
+ 
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
